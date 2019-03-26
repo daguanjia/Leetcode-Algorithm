dc9d8ffd30fe917640b5b8c8e99746dda2b27fc7
daguanjia <leodaguanjia@163.com>
2019-03-26 22:57:01 +0800

    1.0

--- lesson01/index.js
+++ lesson01/index.js
@@ -1,12 +1,38 @@
+// const twoSum = (nums, target) => {
+//     const len = nums.length;
+//     for (let i = 0; i < len; i++) {
+//         const a = target - nums[i];
+//         const found = nums.indexOf(a,i+1);
+//         if(found!== -1){
+//             return [i,found]
+//         }
+//     }
+// };
+// const twoSum = (nums, target) => {
+//     if (nums.length === 2) return [0, 1];
+//     const len = nums.length;
+//     const hashmap={};
+//     for (let i = 0; i < len; i++) {
+//         hashmap[nums[i]]=i;     
+//     }
+//     for (let i = 0; i < len; i++) {
+//         const n = target - nums[i];
+//         const found = hashmap[n];
+//         if(found !== undefined && found != i) return [i, found]; 
+//     }
+//     return [0,0];
+// };
+
 const twoSum = (nums, target) => {
+    if (nums.length === 2) return [0, 1];
     const len = nums.length;
-    for (let i = 0; i < len; i++) {
-        const a = target - nums[i];
-        const found = nums.indexOf(a,i+1);
-        if(found!== -1){
-            return [i,found]
-        }
+    let hashmap = {};
+    for(let i = 0; i < len; i++) {
+        let n = target - nums[i];
+        let find = hashmap[n];
+        if(find !== undefined) return [find, i];
+        else hashmap[nums[i]] = i;
     }
 };
-const nums = [3,4,4];
-console.log(twoSum(nums, 8));
+const nums = [3,4,6];
+console.log(twoSum(nums, 7));
